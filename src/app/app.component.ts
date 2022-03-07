d10edabd913dc62ee7a5a1c1911fe86f14ce2b13
curry <10201039+yong-zhang1217@user.noreply.gitee.com>
2022-03-07 15:14:58 +0800

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
 import { Component } from '@angular/core';
+import { ToastService } from 'ng-zorro-antd-mobile';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
-  title = 'ant-mobile';
+  thumbStyle = {
+    width: '32px',
+    height: '32px'
+  };
+  value = '';
+  user = '';
+  passworld = '';
+  error = false;
+  numberFocus = {
+    focus: false
+  };
+  inputFocus = {
+    focus: false,
+  };
+
+
+  titleFocus = {
+    focus: false,
+  };
+  autoFocus = { focus: true, date: new Date() };
+
+  constructor(private _toast: ToastService) { }
+
+  login() {
+    this._toast.info('login' + this.user);
+  }
+
+  inputErrorClick(e: any) {
+    this._toast.info('Please enter 11 digits');
+  }
+
+  inputChange(e: string) {
+    const value = e.replace(/\s/g, '');
+    if (value.length < 11 && value.length > 0) {
+      this.error = true;
+    } else {
+      this.error = false;
+    }
+    this._toast.info('-->' + e);
+    this.user = e;
+
+  }
+
+  clickFocus() {
+    this.numberFocus = {
+      focus: true,
+    };
+  }
+
+  clickFocusInput() {
+    this.inputFocus = {
+      focus: true,
+    };
+  }
+
+  clickTitle() {
+    this.titleFocus = {
+      focus: true,
+    };
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgZorroAntdMobileModule } from 'ng-zorro-antd-mobile';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    /** 导入 ng-zorro-antd-mobile 模块 **/
+    NgZorroAntdMobileModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
